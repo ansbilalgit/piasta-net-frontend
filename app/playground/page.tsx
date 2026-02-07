47f2a867fee5f37a0bb31471751bf6b4645fbde6
Robel Yonas <robel4872@gmail.com>
2026-02-07 16:25:15 +0100

    add Playground component and Card component files

---
+++ app/playground/page.tsx
@@ -0,0 +1,9 @@
+// app/playground/page.tsx
+export default function Playground() {
+    return (
+        <div className="p-10 flex flex-col gap-5 items-start">
+            <h1>Component Playground</h1>
+            
+        </div>
+    );
+}
